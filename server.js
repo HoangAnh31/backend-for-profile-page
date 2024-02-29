98ac6e91cb58157f3291ce1cd001861be5a13dc9
Nguyen Pham Hoang Anh <anhnph.us@gmail.com>
2024-02-29 21:31:37 +1000

    commit backend

---
+++ server.js
@@ -0,0 +1,51 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+//upload file multipart/form-data
+const multer = require("multer");
+const path = require("path");
+
+//config multer
+const storageConfig = multer.diskStorage({
+  destination: "./images",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.filename + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const app = express();
+const port = 8000;
+
+// Use cors middleware
+app.use(cors());
+
+// Use body-parser middleware to parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const upload = multer({ storage: storageConfig });
+
+//Define a route
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+//receive image from Frontend
+app.post("/upload", upload.single("image"), (req, res) => {
+  const file = req.image;
+  console.log(file);
+  console.log(
+    "-----------------------------------------------------------------------"
+  );
+  console.log(req.body.image);
+
+  res.json({ message: "image has been uploaded successfully" });
+});
+
+//Start the serve
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
